3aeb62662644d6469eadd30e5a11a007cd6d3581
alejandro <jorgerenteriareyes4@gmail.com>
2025-04-03 15:56:28 -0600

    se agregaron rutas para estadisticas

--- prisma/migrations/20250403200049_/migration.sql
+++ prisma/migrations/20250403200049_/migration.sql
@@ -5,4 +5,6 @@
 
 */
 -- AlterTable
-ALTER TABLE `reading` ADD COLUMN `timestamp` DATETIME(3) NOT NULL;
+ALTER TABLE `reading` 
+ADD COLUMN `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
+
---
+++ prisma/migrations/20250403215510_fix_migration/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `reading` ALTER COLUMN `timestamp` DROP DEFAULT;
